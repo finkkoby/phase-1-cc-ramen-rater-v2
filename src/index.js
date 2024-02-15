fa4f5495473503c86482dd37d764071b5305141b
Koby Fink <finkkoby@icloud.com>
2024-02-15 13:59:26 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -3,23 +3,50 @@
 // Callbacks
 const handleClick = (ramen) => {
   let ramenDetail = document.querySelector('div#ramen-detail')
+  let ramenRating = document.querySelector('span#rating-display')
+  let ramenComment = document.querySelector('p#comment-display')
+  
   ramenDetail.children[0].src = ramen.image
   ramenDetail.children[1].innerText = ramen.name
   ramenDetail.children[2].innerText = ramen.restaurant
+
+  ramenRating.innerText = ramen.rating
+  ramenComment.innerText = ramen.comment
 };
 
 const addSubmitListener = () => {
-  // Add code
+  document.querySelector('form#new-ramen').addEventListener('submit', (e) => {
+    e.preventDefault()
+    let formNodes = e.target.children
+    fetch('http://localhost:3000/ramens', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'name': `${formNodes[2].value}`,
+        'restaurant': `${formNodes[4].value}`,
+        'image': `${formNodes[6].value}`,
+        'rating': `${formNodes[8].value}`,
+        'comment': `${formNodes[10].value}`
+      })
+    })
+    .then(response => response.json())
+    .then(object => object)
+    displayRamens()
+  })
 }
 
 const displayRamens = () => {
+  clear()
   fetch('http://localhost:3000/ramens')
   .then(response => response.json())
   .then(ramens => {
     ramens.forEach(ramen => {
       let image = document.createElement('img')
       image.src = ramen.image
-      document.querySelector('div#ramen-menu').appendChild(image)
+      let pics = document.querySelector('div#ramen-menu')
+      pics.appendChild(image)
       image['source'] = {...ramen}
       image.addEventListener('click', e => {
         handleClick(e.target['source'])
@@ -28,9 +55,13 @@ const displayRamens = () => {
   })
 };
 
+const clear = () => {
+  document.querySelector('div#ramen-menu').innerHTML = ''
+}
+
 const main = () => {
   displayRamens()
-  // Invoke addSubmitListener here
+  addSubmitListener()
 }
 
 main()
